9d5f87040ee70c672df7e1d082622d1ee8ffe188
Erlend <40736047+Erlendeikeland@users.noreply.github.com>
2019-10-16 21:02:03 +0200

    Create gamestate.py

---
+++ csgo-gsi-python/gamestate.py
@@ -0,0 +1,10 @@
+import information
+
+class GameState:
+    def __init__(self):
+        self.player = information.Player()
+        self.map = information.Map()
+        self.provider = information.Provider()
+        self.phase_countdowns = information.PhaseCountdowns()
+        self.bomb = information.Bomb()
+        self.round = information.Round()
