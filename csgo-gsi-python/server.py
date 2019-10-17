d2b77473b7b9587a9cf4cb5cea0f3e76d8eca60a
Erlend <40736047+Erlendeikeland@users.noreply.github.com>
2019-10-18 01:34:39 +0200

    Update server.py

--- csgo-gsi-python/server.py
+++ csgo-gsi-python/server.py
@@ -13,27 +13,32 @@ class GSIServer(HTTPServer):
         self.auth_token = auth_token
         self.gamestate = gamestate.GameState()
         self.parser = payloadparser.PayloadParser()
+        
+        self.running = False
 
     def start_server(self):
         try:
             thread = Thread(target=self.serve_forever)
             thread.start()
+            first_time = True
+            while self.running == False:
+                if first_time == True:
+                    print("CS:GO GSI Server starting..")
+                first_time = False
         except:
             print("Could not start server.")
 
     def get_info(self, target, *argv):
         try:
-            if not argv:
-                print("Too few arguments.")
-                return False
+            if len(argv) == 0:
+                state = attrgetter(f"{target}")(self.gamestate)
             elif len(argv) == 1:
                 state = attrgetter(f"{target}.{argv[0]}")(self.gamestate)
             elif len(argv) == 2:
-                state = attrgetter(f"{target}.{argv[0]}.{argv[1]}")(self.gamestate)
+                state = attrgetter(f"{target}.{argv[0]}")(self.gamestate)[f"{argv[1]}"]
             else:
                 print("Too many arguments.")
                 return False
-
             if "object" in str(state):
                 return vars(state)
             else:
@@ -52,6 +57,8 @@ class RequestHandler(BaseHTTPRequestHandler):
         if not self.authenticate_payload(payload):
             print("auth_token does not match.")
             return False
+        else:
+            self.server.running = True
 
         self.server.parser.parse_payload(payload, self.server.gamestate)
 
